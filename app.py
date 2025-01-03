c0bc6b5f6a2efab331ce9a9cf5b66e6ca8cdc807
biel2112 <gabrielc_morais@hotmail.com>
2025-01-03 14:58:02 -0300

    Método de avaliar restaurante como classmethod e classe Avaliacao

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
-from Restaurante import Restaurante
-
+from modelos.restaurante import Restaurante as r
 
 def exibir_subtitulo(texto):
     os.system('clear')
@@ -10,7 +9,6 @@ def exibir_subtitulo(texto):
     print(texto)
     print(linha)
     print()
-
 
 def exibir_nome_programa():
     print("""
@@ -21,13 +19,12 @@ def exibir_nome_programa():
     ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
     ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝\n\n""")
 
-
 def exibir_opcs():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
     print('3. Ativar restaurante')
-    print('4. Sair')
-
+    print('4. Avaliar restaurante')
+    print('5. Sair')
 
 def voltar_menu():
     input("\nPressione Enter para voltar ao menu principal!\n")
@@ -42,27 +39,31 @@ def opc_invalida():
     print("Digite uma opção válida!\n")
     voltar_menu()
 
-
 def escolher_opc():
     try:
         opc = int(input("O que você deseja fazer?\n"))
 
         if opc == 1:
             exibir_subtitulo("Cadastro de novos restaurantes")
-            Restaurante.cadastrar_novo_restaurante()
+            r.cadastrar_novo_restaurante()
             voltar_menu()
 
         elif opc == 2:
             exibir_subtitulo("Lista de restaurantes cadastrados")
-            Restaurante.listar_restaurantes()
+            r.listar_restaurantes()
             voltar_menu()
 
         elif opc == 3:
             exibir_subtitulo("Alterar estado de ativo do restaurante")
-            Restaurante.ativar_restaurante()
+            r.ativar_restaurante()
             voltar_menu()
 
         elif opc == 4:
+            exibir_subtitulo("Área de avaliação de restaurantes")
+            r.avaliar()
+            voltar_menu()
+
+        elif opc == 5:
             finalizar_app()
         else:
             opc_invalida()
@@ -70,13 +71,11 @@ def escolher_opc():
     except:
         opc_invalida()
 
-
 def main():
     os.system('clear')
     exibir_nome_programa()
     exibir_opcs()
     escolher_opc()
-
 
 if __name__ == '__main__':
     main()
---
+++ modelos/restaurante.py
@@ -0,0 +1,73 @@
+from modelos.avaliacao import Avaliacao
+
+class Restaurante:
+
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacao = []
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self._nome.ljust(25)} | {self._categoria.ljust(25)} | {str(self.media_avaliacao).ljust(25)} | {self.ativo}'
+
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} | {"Status"}')
+        for restaurante in Restaurante.restaurantes:
+            print(restaurante)
+
+    @classmethod
+    def cadastrar_novo_restaurante(cls):
+        restaurante_nome = input("Digite o nome do restaurante:\n")
+        restaurante_categoria = input("Digite a categoria desse restaurantes:\n")
+        restaurante = Restaurante(restaurante_nome, restaurante_categoria)
+        print(f'\nRestaurante {restaurante._nome} adicionado com sucesso à lista!\n')
+
+    @classmethod
+    def ativar_restaurante(cls):
+        nome = input("Digite o nome do restaurante que deseja ativar:\n")
+        try:
+            restaurante = next(r for r in Restaurante.restaurantes if r.nome.lower() == nome.lower())
+            restaurante._ativo = not restaurante._ativo
+            msg = f'O restaurante {nome} foi ativado!' if restaurante._ativo else f'O restaurante {nome} foi desativado!'
+            print(msg)
+        except StopIteration:
+            print(f"Restaurante '{nome}' não encontrado!\n")
+
+    @classmethod
+    def avaliar(cls):
+        nome_restaurante = input("Digite o nome dor estaurante que deseja avaliar\n").lower()
+        try:
+            restaurante = next(r for r in Restaurante.restaurantes if r.nome.lower() == nome_restaurante)
+            nome_cliente = input("Digite o seu nome:\n")
+            nota = float(input("Digite uma nota para este restaurante:\n"))
+            avaliacao = Avaliacao(nome_cliente, nota)
+            restaurante._avaliacao.append(avaliacao)
+            print("Avaliação realizada com sucesso!")
+        except StopIteration:
+            print("Restaurante não encontrado!\n")
+
+
+
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return 0
+        else:
+            soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+            qtd_notas = len(self._avaliacao)
+            media_notas = round(soma_notas/qtd_notas, 1)
+            return media_notas
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+
+    @property
+    def nome(self):
+        return self._nome
+
