30a36b2800b1068c17849e84f092ac19a943cf82
biel2112 <gabrielc_morais@hotmail.com>
2025-01-03 17:01:39 -0300

    Adicionando docstrings na classe avaliacao

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,4 +1,10 @@
 class Avaliacao:
     def __init__(self, cliente, nota):
+        """Inicializa uma instância de Avaliacao
+
+        Parâmetros:
+        - nome_cliente (str): Nome do cliente que irá avaliar o restaurante
+        - nota (float): Nota da avaliação do restaurante a ser digitada pelo cliente
+        """
         self._cliente = cliente
         self._nota = nota
