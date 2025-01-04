3fec36961f452755179e9515cb04f685535acefe
biel2112 <gabrielc_morais@hotmail.com>
2025-01-04 12:16:40 -0300

    Adicionando docstrings na classe Bebida

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -3,5 +3,13 @@ from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Bebida(ItemCardapio):
     def __init__(self, nome, preco, tamanho):
+        """Inicializa uma instância de bebida
+
+            Parâmetros:
+            - nome (str): Nome da bebida (Herdado da classe ItemCardapio)
+            - preco (float): Preço da bebida (Herdado da classe Item Cardapio)
+            - tamanho (str): Tamanho da bebida (P, M ou G)
+        """
+
         super().__init__(nome, preco)
         self.tamanho = tamanho
