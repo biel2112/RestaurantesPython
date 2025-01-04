ed3a9bee7709de3b92ed0417a0f0797bc8296120
biel2112 <gabrielc_morais@hotmail.com>
2025-01-04 11:58:33 -0300

    Criando a branch de testes

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -4,7 +4,7 @@ class Avaliacao:
 
         Parâmetros:
         - nome_cliente (str): Nome do cliente que irá avaliar o restaurante
-        - nota (float): Nota da avaliação do restaurante a ser digitada pelo cliente
+        - nota (float): Nota da avaliação do restaurante a ser digitada pelos clientes
         """
         self._cliente = cliente
         self._nota = nota
